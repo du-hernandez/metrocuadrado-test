43b14b95c6702557b9454b0e63927a398e6e5105
Duverney Hernandez Mora <du.hernandez@udla.edu.co>
2021-03-15 22:16:50 -0500

    Approach to router configuration

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,41 @@ import {
     BrowserRouter as Router,
     Switch
 } from 'react-router-dom'
+import { PrivateRoutes, PublicRoutes } from '../common/config'
+import { RoutesMap } from './routes_map'
 
 const HandelrRoutes = () => {
+
+    const { public, private } = RoutesMap
+
+    const publicRoutes = public.map(route => {
+        const { component, exact, path, restricted } = route
+        return (
+            <PublicRoutes
+                component={component}
+                exact={exact}
+                path={path}
+                restricted={restricted}
+            />
+        )
+    })
+
+    const privateRoutes = private.map(route => {
+        const { component, exact, path, restricted } = route
+        return (
+            <PrivateRoutes
+                component={component}
+                exact={exact}
+                path={path}
+                restricted={restricted}
+            />
+        )
+    })
+
     return (
         <Router>
             <Switch>
-                
+                {[...publicRoutes, ...privateRoutes]}
             </Switch>
         </Router>
     )
