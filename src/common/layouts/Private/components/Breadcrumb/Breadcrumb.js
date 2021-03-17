14df83f7e94d0766c9ec49c25914da82f36c3e6e
Duverney Hernandez Mora <du.hernandez@udla.edu.co>
2021-03-16 19:23:22 -0500

    Consistent design. #16
    The route position indicator was styled

--- src/common/layouts/Private/components/Breadcrumb/Breadcrumb.js
+++ src/common/layouts/Private/components/Breadcrumb/Breadcrumb.js
@@ -12,7 +12,7 @@ const CustomeBreadcrumb = () => {
         <Breadcrumb className='private-breadcrumb-layout'>
             {
                 mapRoutes.map(route => (
-                    <Breadcrumb.Item key={route}>
+                    <Breadcrumb.Item key={route} className>
                         {route.replace(/^\w/, (c) => c.toUpperCase())}
                     </Breadcrumb.Item>))
             }
